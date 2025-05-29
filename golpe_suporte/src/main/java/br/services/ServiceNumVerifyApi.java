cf3d7c50614d3e01b88bf1c73b1573ae30051286
bruninhosrs <brunorodrigues398@gmail.com>
2025-05-28 22:03:12 -0300

    Criando Service para API de Validação de Telefone

---
+++ golpe_suporte/src/main/java/br/services/ServiceNumVerifyApi.java
@@ -0,0 +1,67 @@
+package br.services;
+
+import java.io.InputStream;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Properties;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import br.models.HunterApiResponse;
+
+public class ServiceNumVerifyApi {
+    private String apiKey;
+
+    public ServiceNumVerifyApi() {
+        try (InputStream input = getClass().getClassLoader().getResourceAsStream("config.properties")) {
+            Properties prop = new Properties();
+            prop.load(input);
+            apiKey = prop.getProperty("numberverify.api.key");
+            System.out.println("Api key carregada: " + apiKey); // Me mostra minha senha que o Hunter me disponibilizou
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            apiKey = "";
+        }
+
+    }
+
+    public boolean verificarTelefone(String telefone) {
+        try {
+            String urlStr = "http://apilayer.net/api/validate?access_key=" + apiKey + "&number=" + telefone + "&format=1";
+            URI uri = URI.create(urlStr);
+
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(uri)
+                    .GET()
+                    .build();
+
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            int statusCode = response.statusCode();
+            String body = response.body();
+
+            if (statusCode == 200) {
+                ObjectMapper mapper = new ObjectMapper();
+                HunterApiResponse apiResponse = mapper.readValue(body, HunterApiResponse.class);
+
+                String status = apiResponse.getData().getStatus();
+                return "valid".equalsIgnoreCase(status);
+
+            } else if (statusCode == 401) {
+                System.out.println("Erro 401: Chave API inválida ou não autorizado!");
+                return false;
+            } else {
+                System.out.println("Erro na API NumVerify: Código " + statusCode);
+                return false;
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+}
